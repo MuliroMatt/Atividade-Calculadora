86fd44caca17907e1190bdf33b7d24f394f59088
MuliroMatt <murilo.mattiuzzi@gmail.com>
2023-10-10 17:21:05 -0300

    Corrigir

--- Atividade Calculadora/Form1.Designer.cs
+++ Atividade Calculadora/Form1.Designer.cs
@@ -35,11 +35,15 @@
             this.txt1 = new System.Windows.Forms.TextBox();
             this.txt2 = new System.Windows.Forms.TextBox();
             this.lblOper = new System.Windows.Forms.Label();
+            this.btnIgual = new System.Windows.Forms.Button();
+            this.lblResultado = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnLimpar = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // btnMais
             // 
-            this.btnMais.Location = new System.Drawing.Point(37, 164);
+            this.btnMais.Location = new System.Drawing.Point(12, 123);
             this.btnMais.Margin = new System.Windows.Forms.Padding(4);
             this.btnMais.Name = "btnMais";
             this.btnMais.Size = new System.Drawing.Size(96, 32);
@@ -50,62 +54,109 @@
             // 
             // btnMenos
             // 
-            this.btnMenos.Location = new System.Drawing.Point(200, 164);
+            this.btnMenos.Location = new System.Drawing.Point(137, 123);
             this.btnMenos.Margin = new System.Windows.Forms.Padding(4);
             this.btnMenos.Name = "btnMenos";
             this.btnMenos.Size = new System.Drawing.Size(96, 32);
             this.btnMenos.TabIndex = 1;
             this.btnMenos.Text = "-";
             this.btnMenos.UseVisualStyleBackColor = true;
+            this.btnMenos.Click += new System.EventHandler(this.btnMenos_Click);
             // 
             // btnMult
             // 
-            this.btnMult.Location = new System.Drawing.Point(37, 215);
+            this.btnMult.Location = new System.Drawing.Point(12, 163);
             this.btnMult.Margin = new System.Windows.Forms.Padding(4);
             this.btnMult.Name = "btnMult";
             this.btnMult.Size = new System.Drawing.Size(96, 32);
             this.btnMult.TabIndex = 2;
             this.btnMult.Text = "X";
             this.btnMult.UseVisualStyleBackColor = true;
+            this.btnMult.Click += new System.EventHandler(this.btnMult_Click);
             // 
             // btnDiv
             // 
-            this.btnDiv.Location = new System.Drawing.Point(200, 215);
+            this.btnDiv.Location = new System.Drawing.Point(137, 163);
             this.btnDiv.Margin = new System.Windows.Forms.Padding(4);
             this.btnDiv.Name = "btnDiv";
             this.btnDiv.Size = new System.Drawing.Size(96, 32);
             this.btnDiv.TabIndex = 3;
             this.btnDiv.Text = "/";
             this.btnDiv.UseVisualStyleBackColor = true;
+            this.btnDiv.Click += new System.EventHandler(this.btnDiv_Click);
             // 
             // txt1
             // 
-            this.txt1.Location = new System.Drawing.Point(37, 77);
+            this.txt1.Location = new System.Drawing.Point(12, 12);
             this.txt1.Name = "txt1";
             this.txt1.Size = new System.Drawing.Size(100, 29);
             this.txt1.TabIndex = 4;
+            this.txt1.TextChanged += new System.EventHandler(this.txt1_TextChanged);
             // 
             // txt2
             // 
-            this.txt2.Location = new System.Drawing.Point(196, 77);
+            this.txt2.Location = new System.Drawing.Point(137, 12);
             this.txt2.Name = "txt2";
             this.txt2.Size = new System.Drawing.Size(100, 29);
             this.txt2.TabIndex = 5;
+            this.txt2.TextChanged += new System.EventHandler(this.txt2_TextChanged);
             // 
             // lblOper
             // 
             this.lblOper.AutoSize = true;
-            this.lblOper.Location = new System.Drawing.Point(163, 80);
+            this.lblOper.Location = new System.Drawing.Point(118, 15);
             this.lblOper.Name = "lblOper";
             this.lblOper.Size = new System.Drawing.Size(13, 21);
             this.lblOper.TabIndex = 6;
             this.lblOper.Text = ".";
             // 
+            // btnIgual
+            // 
+            this.btnIgual.Location = new System.Drawing.Point(137, 202);
+            this.btnIgual.Name = "btnIgual";
+            this.btnIgual.Size = new System.Drawing.Size(96, 32);
+            this.btnIgual.TabIndex = 7;
+            this.btnIgual.Text = "=";
+            this.btnIgual.UseVisualStyleBackColor = true;
+            this.btnIgual.Click += new System.EventHandler(this.btnIgual_Click);
+            // 
+            // lblResultado
+            // 
+            this.lblResultado.AutoSize = true;
+            this.lblResultado.Location = new System.Drawing.Point(39, 59);
+            this.lblResultado.Name = "lblResultado";
+            this.lblResultado.Size = new System.Drawing.Size(13, 21);
+            this.lblResultado.TabIndex = 8;
+            this.lblResultado.Text = ".";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 59);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(21, 21);
+            this.label1.TabIndex = 9;
+            this.label1.Text = "=";
+            // 
+            // btnLimpar
+            // 
+            this.btnLimpar.Location = new System.Drawing.Point(12, 202);
+            this.btnLimpar.Name = "btnLimpar";
+            this.btnLimpar.Size = new System.Drawing.Size(96, 32);
+            this.btnLimpar.TabIndex = 10;
+            this.btnLimpar.Text = "C";
+            this.btnLimpar.UseVisualStyleBackColor = true;
+            this.btnLimpar.Click += new System.EventHandler(this.btnLimpar_Click);
+            // 
             // Calculadora
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 21F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(336, 506);
+            this.ClientSize = new System.Drawing.Size(251, 249);
+            this.Controls.Add(this.btnLimpar);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.lblResultado);
+            this.Controls.Add(this.btnIgual);
             this.Controls.Add(this.lblOper);
             this.Controls.Add(this.txt2);
             this.Controls.Add(this.txt1);
@@ -135,5 +186,9 @@
         private TextBox txt1;
         private TextBox txt2;
         private Label lblOper;
+        private Button btnIgual;
+        private Label lblResultado;
+        private Label label1;
+        private Button btnLimpar;
     }
 }
--- Atividade Calculadora/Form1.cs
+++ Atividade Calculadora/Form1.cs
@@ -2,14 +2,86 @@ namespace Atividade_Calculadora
 {
     public partial class Calculadora : Form
     {
+        //decimal num1, num2, mais, menos, mult, div, result;
+        double valor1 = 0;
+        double valor2 = 0;
+        double resultado = 0;
+        
         public Calculadora()
         {
             InitializeComponent();
         }
 
+        private void txt1_TextChanged(object sender, EventArgs e)
+        {
+            valor1 = double.Parse(txt1.Text);
+        }
+
+        private void txt2_TextChanged(object sender, EventArgs e)
+        {
+            valor2 = double.Parse(txt2.Text);
+        }
+
         private void btnMais_Click(object sender, EventArgs e)
         {
             lblOper.Text = "+";
+        }
+        private void btnMenos_Click(object sender, EventArgs e)
+        {
+            lblOper.Text = "-";
+        }
+
+        private void btnMult_Click(object sender, EventArgs e)
+        {
+            lblOper.Text = "X";
+        }
+
+        private void btnDiv_Click(object sender, EventArgs e)
+        {
+            lblOper.Text = "/";
+        }
+        private void btnIgual_Click(object sender, EventArgs e)
+        {   if(txt1 == null || txt2 == null)
+            {
+                lblResultado.Text = "PREENCHA OS CAMPOS!";
+            }
+            else
+            {
+                switch (lblOper.Text)
+                {
+                    case "+":
+                        resultado = valor1 + valor2;
+                        lblResultado.Text = resultado.ToString();
+                        break;
+                    case "-":
+                        resultado = valor1 - valor2;
+                        lblResultado.Text = resultado.ToString();
+                        break;
+                    case "/":
+                        resultado = valor1 / valor2;
+                        lblResultado.Text = resultado.ToString();
+                        if (valor2 == 0)
+                        {
+                            lblResultado.Text = "NÃO HÁ DIVISÃO POR 0!";
+                        }
+                        break;
+                    case "X":
+                        resultado = valor1 * valor2;
+                        lblResultado.Text = resultado.ToString();
+                        break;
+                    default:
+                        lblResultado.Text = "ESCOLHA UMA OPERAÇÃO!";
+                        break;
+                }
+            }
+            
+        }
+
+        private void btnLimpar_Click(object sender, EventArgs e)
+        {
+            txt1.Text = "";
+            txt2.Text = "";
+            lblResultado.Text = "";
         }
     }
 }
